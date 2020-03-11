5b16d5b438d0c4c171522b72abc15b6447396681
Quentin Pla <quentin-pla@hotmail.fr>
2020-03-11 17:07:15 +0100

    Création du salon central

---
+++ SalonCentral.java
@@ -0,0 +1,46 @@
+import java.net.*;
+import java.io.*;
+
+public class SalonCentral {
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            try {
+                ServerSocket socketServeur = new ServerSocket(Integer.parseInt(args[0]));
+                System.out.println("# Démarrage du salon central sur le port " + args[0]);
+                while (true) {
+                    Socket socketClient = socketServeur.accept();
+                    new EchoThread(socketClient).start();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class EchoThread extends Thread {
+        private Socket socket;
+        private String pseudo;
+
+        public EchoThread(Socket clientSocket) {
+            this.socket = clientSocket;
+        }
+
+        public void run() {
+            String msg;
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                pseudo = in.readLine();
+                System.out.println("# Connexion de " + pseudo + " au serveur.");
+                while (true) {
+                    msg = in.readLine();
+                    if (msg == null || msg.length() == 0) break;
+                    System.out.println(pseudo + ">" + msg);
+                }
+                System.out.println("# Déconnexion de " + pseudo + ".");
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
